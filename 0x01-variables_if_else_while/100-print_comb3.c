ad92a7b65ef19c541d38cd8786373bc958664625
mohamed271220 <mohamedaprahym@gmail.com>
2023-12-16 04:50:28 +0200

    fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,20 +9,30 @@
 
 int main(void)
 {
-int i, j;
+int i, j, k, l;
+
 for (i = 0; i < 10; i++)
 {
-for (j = i + 1; j < 10; j++)
+for (j = 0; j < 10; j++)
+{
+for (k = i; k < 10; k++)
+{
+for (l = (i == k ? j + 1 : 0); l < 10; l++)
 {
 putchar(i + '0');
 putchar(j + '0');
-if (i < 8)
+putchar(' ');
+putchar(k + '0');
+putchar(l + '0');
+if (!(i == 9 && j == 8 && k == 9 && l == 9))
 {
 putchar(',');
 putchar(' ');
 }
 }
 }
+}
+}
 putchar('\n');
-return (0);
+return 0;
 }
