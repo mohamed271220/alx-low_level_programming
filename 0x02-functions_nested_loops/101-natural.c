21e45d717a3bd581bbb884f0e8ef0132051f91f0
mohamed271220 <mohamedaprahym@gmail.com>
2023-12-19 11:13:58 +0200

    0x02. C - Functions, nested loops:fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,21 +10,19 @@
 
 int main(void)
 {
-    int i, sum;
-
-    sum = 0;
-
-    for (i = 0; i < 1024; i++)
-    {
-        if ((i % 3) == 0)
-        {
-            sum = sum + i;
-        }
-        else if ((i % 5) == 0)
-        {
-            sum = sum + i;
-        }
-    }
-    printf("%d\n", sum);
-    return (0);
+int i, sum;
+sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0)
+{
+sum = sum + i;
+}
+else if ((i % 5) == 0)
+{
+sum = sum + i;
+}
+}
+printf("%d\n", sum);
+return (0);
 }
