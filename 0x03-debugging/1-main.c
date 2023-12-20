94da333766452972361451c3aba93091772a8315
mohamed271220 <mohamedaprahym@gmail.com>
2023-12-20 10:00:54 +0200

    0x03. C - Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int i;
 
  i = 0;
 
-        /*while (i < 10)*/
-        /*{*/
-                /*putchar(i);*/
-        /*}*/
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
 
 printf("Infinite loop avoided! \\o/\n");
 
