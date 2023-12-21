0a485877c7107e7b04f8b3c39b3b9eecd8bfd29a
mohamed271220 <mohamedaprahym@gmail.com>
2023-12-21 14:18:03 +0200

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,29 +1,56 @@
 #include "main.h"
 
 /**
- * main - prints the largest prime factor of the number 612852475143
+ * _sqrt - squares a number
+ * @x: size of the square
  *
- * Return: Always 0.
+ * Return: square root of x
+ */
+
+double _sqrt(int x)
+{
+float sqrt, t;
+sqrt = x / 2;
+t = 0;
+while (sqrt != t)
+{
+t = sqrt;
+sqrt = (x / t + t) / 2;
+}
+return (sqrt);
+}
+
+/**
+ * largest_factor - finds the largest prime factor of a number
+ * @num: number to be factored
+*/
+
+void largest_factor(long int num)
+{
+int prime, largest;
+while (num % 2 == 0)
+num = num / 2;
+for (prime = 3; prime <= _sqrt(num); prime = prime + 2)
+{
+while (num % prime == 0)
+{
+num = num / prime;
+largest = prime;
+}
+}
+if (num > 2)
+largest = num;
+printf("%d\n", largest);
+}
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ *
+ * Return: 0
  */
 
 int main(void)
 {
-long int num = 612852475143;
-long int div = 2;
-long int maxFact;
-while (num != 0)
-{
-if (num % div != 0)
-div = div + 1;
-else
-{
-maxFact = num;
-num = num / div;
-if (num == 1)
-{
-printf("%ld\n", maxFact);
-break;
-}
-}
-}
+largest_factor(612852475143);
+return (0);
 }
