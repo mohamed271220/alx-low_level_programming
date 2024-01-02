07c140e2106d67a5bdf704fb99ee242adeda4bee
mohamed271220 <mohamedaprahym@gmail.com>
2024-01-02 08:55:33 +0200

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: pointer to a string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+}
+s--;
+while (len > 0)
+{
+_putchar(*s);
+s--;
+len--;
+}
+_putchar('\n');
+}
