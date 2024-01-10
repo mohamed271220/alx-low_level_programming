8904e721e4641a3df541d4df43a2dc1549da22ff
mohamed271220 <mohamedaprahym@gmail.com>
2024-01-10 07:57:22 +0200

    0x08. C - Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -12,4 +14,4 @@ int is_prime_number(int n, int i);
 int is_palindrome(char *s, int i, int j);
 int wildcmp(char *s1, char *s2);
 
-#endif // MAIN_H
+#endif /* MAIN_H */
