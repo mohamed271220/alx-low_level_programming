c69424a051ed0c1f90fc4f26b1a80402fa957722
mohamed271220 <mohamedaprahym@gmail.com>
2024-01-10 08:52:04 +0200

    0x08. C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,6 +8,7 @@
  */
 
 int palindrome_check(char *s, int len);
+int _strlen_recursion(char *s);
 int is_palindrome(char *s)
 {
 int len = _strlen_recursion(s);
@@ -30,4 +31,19 @@ else if (*s == *(s + len - 1))
 return (palindrome_check(s + 1, len - 2));
 else
 return (0);
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to be measured
+ *
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
 }
