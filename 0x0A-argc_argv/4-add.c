26c00111d1840eba3be5a28e808153f823fea9b2
mohamed271220 <mohamedaprahym@gmail.com>
2024-01-12 13:19:33 +0200

    0x09. C - Static libraries

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,19 +5,18 @@
  * main - adds positive numbers.
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 
+ * Return: 0
 */
 
 int main(int argc, char const *argv[])
 {
 int sum = 0;
 char *p;
-
 while (--argc)
 {
 for (p = argv[argc]; *p; p++)
 if (*p < '0' || *p > '9')
-return (printf("Error\n"), 1); 
+return (printf("Error\n"), 1);
 sum += atoi(argv[argc]);
 }
 printf("%d\n", sum);
