81fb77bcd0f76b70b8761836a7be46290dcee7a2
mohamed271220 <mohamedaprahym@gmail.com>
2024-01-16 20:44:11 +0200

    0B

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated 
+ * @str: string to copy
+ *
+ * Return: pointer to new string, or NULL if it fails
+ */
+
+char *_strdup(char *str)
+{
+int i = 0, j = 0;
+char *n;
+if (str == NULL)
+return (NULL);
+
+for (; str[i] != '\0'; i++)
+;
+n = malloc(sizeof(*str) * i + 1);
+if (n == 0)
+return (NULL);
+else
+{
+for (; j < i; j++)
+n[j] = str[j];
+}
+return (n);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to new string, or NULL if it fails
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+
+}
