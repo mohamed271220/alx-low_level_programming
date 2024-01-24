ea9ee343e3c77e59cd17da4448557a5048ba3b10
mohamed271220 <mohamedaprahym@gmail.com>
2024-01-24 12:33:01 +0200

    0x0F. C - Function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,14 +12,13 @@
 int main(int argc, char *argv[])
 {
 char *ptr = (char *)main;
-int i, bytes;
+int bytes;
 if (argc != 2)
 printf("Error\n"), exit(1);
 bytes = atoi(argv[1]);
 if (bytes < 0)
 printf("Error\n"), exit(2);
-for (i = 0; i < bytes; i++)
-printf("%02hhx", ptr[i]);
-printf("\n");
+while (bytes--)
+printf("%02hhx%s", *ptr++, bytes ? " " : "\n");
 return (0);
 }
